1128ea5e15c37ea3b0f1c205120ffd7e943a5116
szgabrielchen <szgabrielchen@gmail.com>
2023-06-09 17:00:38 -0400

    first commit

---
+++ src/modules/views/NavbarView.js
@@ -0,0 +1,119 @@
+import React from "react";
+import PropTypes from "prop-types";
+import clsx from "clsx";
+import { withStyles } from "@material-ui/core/styles";
+import Link from "@material-ui/core/Link";
+import AppBar from "../components/AppBar";
+import Toolbar, { styles as toolbarStyles } from "../components/Toolbar";
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+const styles = theme => ({
+  title: {
+    fontSize: 24
+  },
+  placeholder: toolbarStyles(theme).root,
+  toolbar: {
+    justifyContent: "space-between"
+  },
+  left: {
+    flex: 1
+  },
+  leftLinkActive: {
+    color: theme.palette.common.white
+  },
+  right: {
+    flex: 1,
+    display: "flex",
+    justifyContent: "flex-end"
+  },
+  rightLink: {
+    fontSize: 16,
+    color: theme.palette.common.white,
+    marginLeft: theme.spacing(3)
+  },
+  linkSecondary: {
+    color: theme.palette.secondary.main
+  }
+});
+
+const NavbarView = ({ classes }) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <div>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          <div className={classes.leftLinkActive} />
+          <Link
+            variant='caption'
+            underline="none"
+            color="inherit"
+            className={classes.title}
+            href="/premium-themes/onepirate/"
+          >
+            {"K-W Recreational Baseball"}
+          </Link>
+          <div className={classes.right}>
+            <div>
+              <Button className={classes.rightLink} aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                  Menu
+              </Button>
+              <Menu
+                  id="simple-menu"
+                  anchorEl={anchorEl}
+                  keepMounted
+                  open={Boolean(anchorEl)}
+                  onClose={handleClose}
+              >
+                  <MenuItem onClick={handleClose}>Home</MenuItem>
+                  <MenuItem onClick={handleClose}>League Introduction</MenuItem>
+                  <MenuItem onClick={handleClose}>Rosters 2023</MenuItem>
+                  <MenuItem onClick={handleClose}>Stats</MenuItem>
+                  <MenuItem onClick={handleClose}>Game Logs</MenuItem>
+                  <MenuItem onClick={handleClose}>History</MenuItem>
+                  <MenuItem onClick={handleClose}>Rules</MenuItem>
+                  <MenuItem onClick={handleClose}>Registration</MenuItem>
+                  <MenuItem onClick={handleClose}>FAQ</MenuItem>
+                  <MenuItem onClick={handleClose}>Contact Us</MenuItem>
+                  <MenuItem onClick={handleClose}>Links</MenuItem>
+              </Menu>
+            </div>
+            {/* <Link
+              color="inherit"
+              variant="h6"
+              underline="none"
+              className={classes.rightLink}
+              href="/premium-themes/onepirate/sign-in/"
+            >
+              {"Sign In"}
+            </Link> */}
+            {/* <Link
+              variant="h6"
+              underline="none"
+              className={clsx(classes.rightLink, classes.linkSecondary)}
+              href="/premium-themes/onepirate/sign-up/"
+            >
+              {"Sign Up"}
+            </Link> */}
+          </div>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.placeholder} />
+    </div>
+  );
+};
+
+NavbarView.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(NavbarView);
---
+++ src/modules/views/ProductCategories.js
@@ -0,0 +1,163 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import ButtonBase from "@material-ui/core/ButtonBase";
+import Container from "@material-ui/core/Container";
+import Typography from "../components/Typography";
+import whiteSox from './whiteSox.jpeg';
+import expos from './expos.webp';
+import blueJays from './blueJays.jpeg';
+import dodgers from './dodgers.jpeg';
+
+const styles = theme => ({
+  root: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(4)
+  },
+  images: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  imageWrapper: {
+    position: "relative",
+    display: "block",
+    padding: 0,
+    borderRadius: 0,
+    height: "40vh",
+    [theme.breakpoints.down("sm")]: {
+      width: "100% !important",
+      height: 100
+    },
+    "&:hover": {
+      zIndex: 1
+    },
+    "&:hover $imageBackdrop": {
+      opacity: 0.15
+    },
+    "&:hover $imageMarked": {
+      opacity: 0
+    },
+    "&:hover $imageTitle": {
+      border: "4px solid currentColor"
+    }
+  },
+  imageButton: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    color: theme.palette.common.white
+  },
+  imageSrc: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundSize: "cover",
+    backgroundPosition: "center 40%"
+  },
+  imageBackdrop: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    background: theme.palette.common.black,
+    opacity: 0.5,
+    transition: theme.transitions.create("opacity")
+  },
+  imageTitle: {
+    position: "relative",
+    padding: `${theme.spacing(2)}px ${theme.spacing(4)}px 14px`
+  },
+  imageMarked: {
+    height: 3,
+    width: 18,
+    background: theme.palette.common.white,
+    position: "absolute",
+    bottom: -2,
+    left: "calc(50% - 9px)",
+    transition: theme.transitions.create("opacity")
+  }
+});
+
+function ProductCategories(props) {
+  const { classes } = props;
+
+  const images = [
+    {
+      url:
+      whiteSox,
+      title: "White Sox",
+      width: "25%"
+    },
+    {
+      url:
+        blueJays,
+      title: "Blue Jays",
+      width: "25%"
+    },
+    {
+      url:
+        dodgers,
+      title: "Dodgers",
+      width: "25%"
+    },
+    {
+      url:
+        expos,
+      title: "Expos",
+      width: "25%"
+    },
+  ];
+
+  return (
+    <Container className={classes.root} component="section">
+      <Typography variant="h4" marked="center" align="center" component="h2">
+        Rosters 2023
+      </Typography>
+      <div className={classes.images}>
+        {images.map(image => (
+          <ButtonBase
+            key={image.title}
+            className={classes.imageWrapper}
+            style={{
+              width: image.width
+            }}
+          >
+            <div
+              className={classes.imageSrc}
+              style={{
+                backgroundImage: `url(${image.url})`
+              }}
+            />
+            <div className={classes.imageBackdrop} />
+            <div className={classes.imageButton}>
+              <Typography
+                component="h3"
+                variant="h6"
+                color="inherit"
+                className={classes.imageTitle}
+              >
+                {image.title}
+                <div className={classes.imageMarked} />
+              </Typography>
+            </div>
+          </ButtonBase>
+        ))}
+      </div>
+    </Container>
+  );
+}
+
+ProductCategories.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(ProductCategories);
